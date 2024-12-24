bcc6f43a51ff2e500cee8eb70a6069cdd68918ef
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-12-24 11:43:28 +0530

    some changes

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 require('dotenv').config()
-const express = require("express");  // it is a commonJS module syntax (for server side env) 
-//     OR ES module syntax (it is more modern js module)
-// import express from "express"
+const express = require("express");  
 
-// NOTE:- here const express is an object we created 
 
 const app = express();
 
-// NOTE: here app is a powerful variable which consist of many things now like
-//  app.get , app.listen just like MATH comes with many things like : pow , pi , sum etc
-
-const port =  4000;  // virtual port where we want express to listen just like we have physical port type C port
 
 
-  // req --> request  , res--> response we send when getting request 
-app.get("/", (req, res) => {  // making a get request with "/"--> home route
-  res.send("Hello World!");  // when getting request at home route sending response as string "hello world"
+const port =  4000;  
+
+
+  
+app.get("/", (req, res) => { 
+  res.send("Hello World!");  
 });
 
-// handling multiple request
+
 
 app.get('/twitter', (req , res)=> {
       res.send('sukhiidotcom')
@@ -38,9 +34,6 @@ app.get('/github', (req,res)=> {
 })
 
 
-//  using listening method from express and enabling app to listen at port
-// on successful listening displaying message in console log
-// listen method is only a variable which is in port 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
